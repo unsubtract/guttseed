b0258e80dd1df7da7a3da22d80fc178feef4bf8c
unsubtract <saihaj.sran@gmail.com>
2022-09-10 12:55:02 -0400

    divisionless version (it's a lot faster)

--- hexmsm_rng.c
+++ hexmsm_rng.c
@@ -21,22 +21,16 @@ uintmax_t hexmsm_next(uintmax_t n)
     uintmax_t left = difference >> 1;
     uintmax_t right = difference - left;
  /* printf("diff: %lu, left: %lu, right: %lu\n", difference, left, right); */
-
-    n %= hex_pow10(sqrlen - left);
+    
+    /* for left, make a big mask covering only the digits we want
+     * then for right, simply shift off the digits */
+    n &= (0x1 << (sqrlen - left) * NYBBLE) - 1;
     n >>= right * NYBBLE;
     return n;
 }
-
 
 uintmax_t hex_digits(uintmax_t n)
 {
     if (n < 0x10) return 1;
     return 1 + hex_digits(n >> NYBBLE);
-}
-
-uintmax_t hex_pow10(uintmax_t n)
-{
-    /* left-shifting by n digits is equal to repeated multiplication by 0x10
-     * this is much, much faster than what the compiler otherwise produces */
-    return 0x1 << (n * NYBBLE);
 }
